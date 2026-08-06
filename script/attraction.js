33454b00bac61e7d85227ea7c92e685f4f56444c
llldbals-hue <llldbals@gmail.com>
2026-08-06 17:36:19 +0900

    2026-08-06 스크립트 추가

---
+++ script/attraction.js
@@ -0,0 +1,39 @@
+$(function () {
+
+  $(".dropdown-bg").css({
+    position: "absolute",
+    top: "100%",
+    left: 0,
+    zIndex: 1,
+    width: "100%",
+    height: 0,
+    backgroundColor: "#fff",
+    borderTop: "1px solid #eee",
+    boxShadow: "0 12px 24px rgba(0, 0, 0, 0.08)"
+  });
+
+  $(".submenu").hide();
+
+  $(".gnb").mouseenter(function () {
+    $(".dropdown-bg")
+      .stop()
+      .animate({ height: "250px" }, 250);
+
+    $(".submenu")
+      .stop()
+      .fadeIn(200);
+  });
+
+  $("header").mouseleave(function () {
+    $(".dropdown-bg")
+      .stop()
+      .animate({ height: 0 }, 250);
+
+    $(".submenu")
+      .stop()
+      .fadeOut(200);
+  });
+
+
+ /////////////////// 종료 //////////////////
+});
